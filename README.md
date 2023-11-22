c69811735ceec4e7525ea24676900ecb83b9dd05
Matt Sweet <themattbook@gmail.com>
2023-11-22 13:58:00 -0600

    Updated configs, removed unneeded junk, install script

---
+++ README.md
@@ -0,0 +1,13 @@
+# My Hyprland Config
+This is a modified clone of SolDoesTech's original Hyprland setup, found [here](https://github.com/SolDoesTech/hyprland)
+
+## Changelog
+Departing from the original SDT setup, I made the following modifications:
+
+- Removed the wifi powersave prompt from the setup script
+- Modified the configs to remove the bluetooth, unneeded network information, and reduced workspace count in the tray
+- Updated the font to account for the improperly displayed icons
+- Removed the ASUS-specific setup from the original installation script
+
+## Installation
+To install, simply clone the repo and run the ```set-hypr``` script.
---
+++ set-hypr.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# The follwoing will attempt to install all needed packages to run Hyprland
+# This is a quick and dirty script there are no error checking
+# This script is meant to run on a clean fresh system
+#
+# Below is a list of the packages that would be installed
+#
+# hyprland: This is the Hyprland compositor
+# kitty: This is the default terminal
+# waybar: Waybar now has hyprland support
+# swaybg: This is used to set a desktop background image
+# swaylock-effects: This allows for the locking of the desktop its a fork that adds some editional visual effects
+# wofi: This is an application launcher menu
+# wlogout: This is a logout menu that allows for shutdown, reboot and sleep
+# mako: This is a graphical notification daemon
+# thunar: This is a graphical file manager
+# ttf-jetbrains-mono-nerd: Som nerd fonts for icons and overall look
+# noto-fonts-emoji: fonts needed by the weather script in the top bar
+# polkit-gnome: needed to get superuser access on some graphical appliaction
+# python-requests: needed for the weather module script to execute
+# starship: allows to customize the shell prompt
+# swappy: This is a screenshot editor tool
+# grim: This is a screenshot tool it grabs images from a Wayland compositor
+# slurp: This helps with screenshots, it selects a region in a Wayland compositor
+# pamixer: This helps with audio settings such as volume
+# gvfs: adds missing functionality to thunar such as automount usb drives
+# lxappearance: used to set GTK theme
+# xfce4-settings: set of tools for xfce, needed to set GTK theme
+# xdg-desktop-portal-hyprland: xdg-desktop-portal backend for hyprland
+
+#### Check for yay ####
+ISYAY=/sbin/yay
+if [ -f "$ISYAY" ]; then 
+    echo -e "yay was located, moving on.\n"
+    yay -Suy
+else 
+    echo -e "yay was not located, please install yay. Exiting script.\n"
+    exit 
+fi
+
+### Install all of the above pacakges ####
+read -n1 -rep 'Would you like to install the packages? (y,n)' INST
+if [[ $INST == "Y" || $INST == "y" ]]; then
+    yay -S --noconfirm hyprland kitty waybar \
+    swaybg swaylock-effects wofi wlogout mako thunar \
+    ttf-jetbrains-mono-nerd noto-fonts-emoji \
+    polkit-gnome python-requests starship \
+    swappy grim slurp pamixer gvfs \
+    lxappearance xfce4-settings \
+    xdg-desktop-portal-hyprland
+    
+    # Clean out other portals
+    echo -e "Cleaning out conflicting xdg portals...\n"
+    yay -R --noconfirm xdg-desktop-portal-gnome xdg-desktop-portal-gtk
+fi
+
+### Copy Config Files ###
+read -n1 -rep 'Would you like to copy config files? (y,n)' CFG
+if [[ $CFG == "Y" || $CFG == "y" ]]; then
+    echo -e "Copying config files...\n"
+    cp -R hypr ~/.config/
+    cp -R kitty ~/.config/
+    cp -R mako ~/.config/
+    cp -R waybar ~/.config/
+    cp -R swaylock ~/.config/
+    cp -R wofi ~/.config/
+    
+    # Set some files as exacutable 
+    chmod +x ~/.config/hypr/xdg-portal-hyprland
+    chmod +x ~/.config/waybar/scripts/waybar-wttr.py
+fi
+
+### Install teh starship shell ###
+read -n1 -rep 'Would you like to install the starship shell? (y,n)' STAR
+if [[ $STAR == "Y" || $STAR == "y" ]]; then
+    # install the starship shell
+    echo -e "Updating .bashrc...\n"
+    echo -e '\neval "$(starship init bash)"' >> ~/.bashrc
+    echo -e "copying starship config file to ~/.confg ...\n"
+    cp starship.toml ~/.config/
+fi
+
+### Script is done ###
+echo -e "Script had completed.\n"
+echo -e "You can start Hyprland by typing Hyprland (note the capital H).\n"
+read -n1 -rep 'Would you like to start Hyprland now? (y,n)' HYP
+if [[ $HYP == "Y" || $HYP == "y" ]]; then
+    exec Hyprland
+else
+    exit
+fi
